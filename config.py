085ee718b213c2a42c5c420e01665d5b29b4e2a1
IMrProYTI <IMrProYTI@users.noreply.github.com>
2020-12-22 22:57:16 +0300

    Function update

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 special_symbol = "$"
+tags_divider = 'tab'
+sleeptime = 0.1
 button = "f1"
 LANG = "EN"
--- main.py
+++ main.py
@@ -1,60 +1,14 @@
 from keyboard import is_pressed, write, send
+from interface import interface
 from time import sleep
 from os import system
 import config
 
 status = False
-version = "v0.5"
 TAGS = open("TAGS.txt", "r")
-system("mode con cols=65 lines=15")
+system("mode con cols=70 lines=15")
 
-if config.LANG == "EN":
-    print('TAGS writer', version)
-
-    print()
-    print('Name anime/film:')
-    name1 = input('First variant of name: ')
-    name2 = input('Second variant of name: ')
-    name3 = input('Third variant of name: ')
-
-    print()
-    print('Name of track:')
-    track1 = input('First variant of name: ')
-    track2 = input('Second variant of name: ')
-    track3 = input('Third variant of name: ')
-
-    print()
-    print("Press button for use script:", config.button)
-
-if config.LANG == "RU":
-    print('TAGS писатель', version)
-
-    print()
-    print('Название аниме/фильма:')
-    name1 = input('Первый вариант названия: ')
-    name2 = input('Второй вариант названия: ')
-    name3 = input('Третий вариант названия: ')
-
-    print()
-    print('Название трека:')
-    track1 = input('Первый вариант названия: ')
-    track2 = input('Второй вариант названия: ')
-    track3 = input('Третий вариант названия: ')
-
-    print()
-    print("Кнопка для использования скрипта:", config.button)
-
-if config.LANG == "DEBUG":
-    name1 = "name1"
-    name2 = "name2"
-    name3 = "name3"
-
-    track1 = "track1"
-    track2 = "track2"
-    track3 = "track3"
-
-    print("Press button for use script:", config.button)
-
+interface()
 while True:
     if is_pressed(config.button):
         status = True
@@ -66,33 +20,33 @@ while True:
             if line == '$':
                 if name1 != "":
                     write(name1) # name1
-                    send("tab")
-                    sleep(0.1)
+                    send(tags_divider)
+                    sleep(config.sleeptime)
                 if name2 != "":
                     write(name2) # name2
-                    send("tab")
-                    sleep(0.1)
+                    send(tags_divider)
+                    sleep(config.sleeptime)
                 if name3 != "":
                     write(name3) # name3
-                    send("tab")
-                    sleep(0.1)
+                    send(tags_divider)
+                    sleep(config.sleeptime)
 
                 if track1 != "":
                     write(track1) # track1
-                    send("tab")
-                    sleep(0.1)
+                    send(tags_divider)
+                    sleep(config.sleeptime)
                 if track2 != "":
                     write(track2) # track2
-                    send("tab")
-                    sleep(0.1)
+                    send(tags_divider)
+                    sleep(config.sleeptime)
                 if track3 != "":
                     write(track3) # track3
-                    send("tab")
-                    sleep(0.1)
+                    send(tags_divider)
+                    sleep(config.sleeptime)
             else:
                 write(line)
-                send("tab")
-                sleep(0.1)
+                send(tags_divider)
+                sleep(config.sleeptime)
         TAGS.close()
         if config.LANG == "RU":
             print('Программа закроеться через 10 секунд.')
